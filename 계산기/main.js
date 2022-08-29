181aacf095ed96fdc02c66e97443b62288d63c93
aquaman122 <89385423+aquaman122@users.noreply.github.com>
2022-08-29 11:59:09 +0900

    2022_08_29

--- 계산기/main.js
+++ 계산기/main.js
@@ -28,6 +28,50 @@ document.querySelector('#num-8').addEventListener('click', onClickNumber);
 document.querySelector('#num-9').addEventListener('click', onClickNumber);
 
 const onClickOperator = (op) => () => {
+  if(op === '-') { // op가 - 일때
+    if(!numOne) { // operator 가  비었을때
+      numOne += op;
+      $result.value += op;
+      return;
+    } else if (numOne && !operator) {
+      operator = op;
+      $operator.value = op;
+      return;
+    }
+    if (!numTwo) { // numTwo가 비엇을때
+      $result.value = '';
+      numTwo += op;
+      $result.value = op;
+      return;
+    }
+  } else { // op가 -가 아닐때
+    if (!numOne) { //numOne이 비어있을때
+      alert('숫자를 입력하세요.');
+    } else { //numOne이 있을때
+      operator = op;
+      $operator.value = op;
+    }
+  }
+  if (numTwo) {
+    switch (operator) {
+      case '+':
+        $result.value = parseInt(numOne) + parseInt(numTwo);
+        break;
+      case '-':
+        $result.value = numOne - numTwo;
+        break;
+      case '*':
+        $result.value = numOne * numTwo;
+        break;
+      case '/':
+        $result.value = numOne / numTwo;
+        break;
+      default:
+        break;
+    }
+    numOne = $result.value;
+    numTwo = '';
+  }
   if (numOne) {
     operator = op;
     $operator.value = op;
