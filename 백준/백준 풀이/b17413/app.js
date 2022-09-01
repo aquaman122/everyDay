6e86b866b6b7dc440ecdef8d9fbab6c7419487ae
aquaman122 <89385423+aquaman122@users.noreply.github.com>
2022-09-01 09:18:25 +0900

    Update app.js

--- 백준/백준 풀이/b17413/app.js
+++ 백준/백준 풀이/b17413/app.js
@@ -1,13 +1,14 @@
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-let input = fs.readFileSync(filePath).toString().split('\n');
+let input = fs.readFileSync(filePath).toString().split(' ');
 
+for(let i =0; i<input.length; i++) {
+    let words = input[i].split(' ');
+    let answer = words.map((word) => word.split('').reverse().join('')).join(' ');
+    console.log(answer);
+}
 
 function solution() {
     // write here
-    
-    for (let i=0; i<input.length; i++) {
-        let str = input[i].split(' ');
-        console.log('str');
-    }
+
 }
