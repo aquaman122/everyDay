4c321a74d332947769bb49fcd5d985bd424cf784
aquaman122 <89385423+aquaman122@users.noreply.github.com>
2022-10-14 13:04:29 +0900

    2022/10/14
    webpack build

---
+++ lecturegugu/client.jsx
@@ -0,0 +1,9 @@
+const React = require('react');
+const ReactDom = require('react-dom');
+
+const GuGuDan = require('./gugudan');
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(
+  <GuGuDan/>
+);
