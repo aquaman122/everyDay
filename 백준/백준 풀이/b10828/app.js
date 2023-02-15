83dcd2da2208568d8d549a96d68bfceca8dff54e
aquaman <swj981128@gmail.com>
2023-02-15 21:53:52 +0900

    20230215

--- 백준/백준 풀이/b10828/app.js
+++ 백준/백준 풀이/b10828/app.js
@@ -9,32 +9,26 @@ solution(test)
 function solution(test) {
     // write here
     // 빈 배열
-    const stack = [];
-    const result = [];
-    
-    for (let i = 0; i < test; i++ ) {
-        switch (input[i]) {
-            case 'pop' :
-               result.push(stack.pop() || -1);
-               break;
-
-            case 'size' : 
-                result.push(stack.length);
-                break;
-
-            case 'empty' : 
-                result.push(stack[0] ? 0 : 1);
-                break;
-            
-            case 'top' : 
-                result.push(stack[stack.length -1] || -1)
-                break;
-
-            default: 
-                stack.push(input[i].split(' ')[1]);
-                break;
+    const array = [];
+    const answer = [];
+    for (let i =0; i < test; i++) {
+        switch(input[i]) {
+            case 'pop':
+              array.length === 0 ? answer.push(-1) : answer.push(array.pop());
+              break;
+            case 'size':
+              answer.push(array.length);
+              break;
+            case 'empty':
+              array.length === 0 ? answer.push(1) : answer.push(0);
+              break;
+            case 'top':
+              array.length === 0 ? answer.push(-1) : answer.push(array[array.length - 1]);
+              break;
+            default:
+              array.push(input[i].split(' ')[1]);
+              break;
         }
     }
-
-    console.log(result.join(' '));
+    console.log(answer.join('\n'));
 }
