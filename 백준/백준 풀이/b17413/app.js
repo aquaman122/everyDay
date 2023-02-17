f5ab1aa1d96a15dcd4b512ed740d20e5335012bb
aquaman <swj981128@gmail.com>
2023-02-17 14:09:01 +0900

    20230217

--- 백준/백준 풀이/b17413/app.js
+++ 백준/백준 풀이/b17413/app.js
@@ -2,13 +2,10 @@ const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 let input = fs.readFileSync(filePath).toString().split(' ');
 
-for(let i =0; i<input.length; i++) {
-    let words = input[i].split(' ');
-    let answer = words.map((word) => word.split('').reverse().join('')).join(' ');
-    console.log(answer);
-}
+solution();
 
 function solution() {
     // write here
+    
 
 }
