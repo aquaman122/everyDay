6593dfcd66d78a8354bcc4e1c49dbf7fb1398ece
aquaman <swj981128@gmail.com>
2023-02-17 11:09:00 +0900

    20230217

---
+++ 백준/백준 풀이/b4949/app.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const { arrayBuffer } = require('stream/consumers');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
+
+
+solution()
+
+function solution() {
+    // write here
+    
+}
