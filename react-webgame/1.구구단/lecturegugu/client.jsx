77517bcb72a1aebf7d4215c5aefcf89026c7bc2b
aquaman <swj981128@gmail.com>
2023-02-19 12:31:57 +0900

    20230219

--- react-webgame/1.구구단/lecturegugu/client.jsx
+++
@@ -1,9 +0,0 @@
-const React = require('react');
-const ReactDom = require('react-dom');
-
-const GuGuDan = require('./gugudan');
-
-const root = ReactDOM.createRoot(document.querySelector('#root'));
-root.render(
-  <GuGuDan/>
-);
--- 백준/백준 풀이/b1000/app.js
+++ 백준/백준 풀이/b1000/app.js
@@ -1,11 +1,23 @@
 const fs = require('fs');
+const { type } = require('os');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 let input = fs.readFileSync(filePath).toString().split('\n');
 
+let currentTime = input[0];
+let [h, m] = currentTime.split(' ').map((x) => +x);
+let time = +input[1];
 
+solution(h, m, time);
 
-solution();
-
-function solution() {
-    // write here
+function solution(h, m, time) {
+    // write here  
+    m += time;
+    if (m >= 60) {
+        h += Math.floor(m / 60);
+        m %= 60;
+        if (h > 23) {
+            h -= 24;
+        }
+    }
+    console.log(h, m);
 }
