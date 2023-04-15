7b857a82eaa00995dba5ca16a65e691e4d75f45c
aquaman <swj981128@gmail.com>
2023-04-15 16:01:58 +0900

    0

--- 백준/백준 풀이/b1000 copy/app.js
+++ 백준/백준 풀이/b1000 copy/app.js
@@ -3,8 +3,21 @@ const { type } = require('os');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 let input = fs.readFileSync(filePath).toString().trim().split('\n');
 
-solution();
+let [n, k] = input[0].split(' ').map((v) => +v);
 
-function solution() {
-    
+solution(n, k);
+
+function solution(n, k) {
+  let answer = [];
+  let arr = new Array(n).fill().map((v, i) => i +1);
+  for (let i = 0; i < n; i++) {
+    for (let j =1; j <= k; j++) {
+      if( j === k ) {
+        answer.push(arr.shift());
+      } else {
+        arr.push(arr.shift());
+      }
+    }
+  }
+  console.log(answer);
 }
