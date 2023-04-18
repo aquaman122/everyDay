bc03e752d537120c28402ca48872ed6e846e1cd9
aquaman <swj981128@gmail.com>
2023-04-18 20:41:33 +0900

    0

---
+++ 백준/백준 풀이/b16170/app.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const { type } = require('os');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+const date = new Date();
+const year = date.getFullYear();
+const month = date.getMonth();
+const day = date.getDate();
+
+console.log(`${year}\n${month}\n${day}`);
