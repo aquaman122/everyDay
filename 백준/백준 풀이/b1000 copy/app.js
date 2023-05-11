3db055494e4e37b46b358a32059190d292627cdc
aquaman <swj981128@gmail.com>
2023-05-11 20:20:08 +0900

    1

---
+++ 백준/백준 풀이/b1000 copy/app.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const { type } = require('os');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+solution();
+
+function solution() {
+    
+}fdsaafsd
