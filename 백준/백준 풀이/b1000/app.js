c32b2b6d881a8e51a3d724a95a8cc67934972608
aquaman <swj981128@gmail.com>
2023-09-04 11:52:03 +0900

    .

--- 백준/백준 풀이/b1000/app.js
+++ 백준/백준 풀이/b1000/app.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 const { type } = require('os');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-let input = fs.readFileSync(filePath).toString().trim().split('\n');
+let input = fs.readFileSync(filePath).toString().trim().split(' ');
 
 solution();
 
 function solution() {
-    
+  console.log(Number(input[0]) + Number(input[1]));
 }
--- 백준/백준 풀이/b1002/app.js
+++ 백준/백준 풀이/b1002/app.js
@@ -8,31 +8,5 @@ const T = +input.shift();
 solution(T);
 
 function solution(T) {
-  for (let i = 0 ; i< T; i++) {
-    let [x1, y1, r1, x2, y2, r2] = input[i].split(' ').map(v => +v);
-    const dx = x1 - x2;
-    const dy = y1 - y2;
-    if (r1 > r2)  {
-      const temp = r1;
-      r1 = r2;
-      r2 = temp;
-    }
-    const rSum = (r1 + r2) * (r1 + r2);
-    const rSub = (r2 - r1) * (r2 - r1);
-    const d = dx * dx + dy * dy;
-
-    if (d < rSum && d > rSub) {
-      console.log(2);
-    } else if (d === rSum || (d === rSub && d !== 0)) {
-      console.log(1);
-    } else if (d < rSub || d > rSum) {
-      console.log(0);
-    } else if (d === 0) {
-      if (r1 === r2) {
-        console.log(-1);
-      } else {
-        console.log(0);
-      }
-    }
-  }
+  
 }
