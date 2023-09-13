742e4c852335272cd26d952002dadf2dd805ffb7
aquaman <swj981128@gmail.com>
2023-09-13 18:34:51 +0900

    .

--- 백준/백준 풀이/b9498/app.js
+++ 백준/백준 풀이/b9498/app.js
@@ -2,23 +2,9 @@ const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 let input = fs.readFileSync(filePath).toString().split('\n');
 
-input = input[0];
-input = input.split(' ').map((item) => +item);
+solution();
 
-solution(input[0], input[1]);
-
-function solution(A, B) {
+function solution() {
     // write here
     
-    if ( 100 >= A && A  >= 90) {
-        console.log("A");
-    } else if (89 >= A && A >= 80) {
-        console.log("B");
-    } else if (79 >= A && A >= 70) {
-        console.log("C");
-    } else if (69 >= A && A  >= 60) {
-        console.log("D");
-    } else {
-        console.log("F");
-    }
 }
