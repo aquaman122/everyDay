554e273261d4eef50b768cf5e340233a8ed86001
aquaman <swj981128@gmail.com>
2023-09-18 21:18:20 +0900

    backjoon

--- 백준/백준 풀이/b25206/app.js
+++ 백준/백준 풀이/b25206/app.js
@@ -1,46 +1,66 @@
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-let input = fs.readFileSync(filePath).toString().trim().split('\n').map(v => v.split(' '));
+let input = fs.readFileSync(filePath).toString().trim().split('\n');
 
 solution(input)
 
 function solution(input) {
     // write here
-    let score = 0;
-    let rank = [];
-    let rankscore = 0;
-    let pCount = 0;
-    let aScore = 0;
-    let sAvg = 0;
-    for (let i =0; i < input.length; i++) {
-        score += Number(input[i][1]);
-        rank.push(input[i][2]);
 
-        switch (rank[i]) {
-            case 'A+': rankscore = 4.5;
-            break;
-            case 'A0': rankscore = 4.0;
-            break;
-            case 'B+': rankscore = 3.5;
-            break;
-            case 'B0': rankscore = 3.0;
-            break;
-            case 'C+': rankscore = 2.5;
-            break;
-            case 'C0': rankscore = 2.0;
-            break;
-            case 'D+': rankscore = 1.5;
-            break;
-            case 'D0': rankscore = 1.0;
-            break;
-            case 'F': rankscore = 0.0;
-            break;
-            case 'P': pCount++;
-            break;
-            default:
-            break;
-        }
-        sAvg += Number(input[i][1]) + rankscore;
+    // const PxInput = input.filter((item) => item.split(' ')[2] !== 'P');
+
+    // let scoreSum = 0;
+    // let getScores = 0;
+    // PxInput.map((item, i) => {
+    //     const [subject, score, rank] = item.split(' ');
+    //     let rankScore = 0;
+    //     if (rank === 'A+') {
+    //         rankScore = 4.5;
+    //     } else if (rank === 'A0') {
+    //         rankScore = 4;
+    //     } else if (rank === 'B+') {
+    //         rankScore = 3.5;
+    //     } else if (rank === 'B0') {
+    //         rankScore = 3;
+    //     } else if (rank === 'C+') {
+    //         rankScore = 2.5;
+    //     } else if (rank === 'C0') {
+    //         rankScore = 2.0;
+    //     } else if (rank === 'D+') {
+    //         rankScore = 1.5;
+    //     } else if (rank === 'D0') {
+    //         rankScore = 1;
+    //     } else if (rank === 'F') {
+    //         rankScore = 0;
+    //     }
+    //     scoreSum += Number(score);
+    //     getScores += Number(score) * rankScore
+    // });
+    // console.log(getScores / scoreSum);
+    
+    let scoreSum = 0;
+    let getScores = 0;
+
+    const subScore = {
+        "A+": 4.5,
+        "A0": 4.0,
+        "B+": 3.5,
+        "B0": 3.0,
+        "C+": 2.5,
+        "C0": 2.0,
+        "D+": 1.5,
+        "D0": 1.0,
+        "F": 0.0
     }
-    console.log(sAvg);
+    input.forEach(item => {
+        const [subject, score, rank] = item.split(' ');
+
+        if (rank !== 'P') {
+            const rankScore = subScore[rank];
+
+            scoreSum += Number(score);
+            getScores += Number(rankScore) * score;
+        }
+    })
+    console.log(getScores / scoreSum);
 }
